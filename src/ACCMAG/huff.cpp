a6ddcf8101162472b40b730b51842659521ebccf
Tapuwa-Dhliwayo <48251460+Tapuwa-Dhliwayo@users.noreply.github.com>
2019-09-30 16:12:12 +0200

    decoder being moved

--- src/ACCMAG/huff.cpp
+++ src/ACCMAG/huff.cpp
@@ -103,33 +103,6 @@ void writer(std::string output, std::string encoded_data, std::unordered_map<cha
 
 }
 
-void decoder(std::string filename){
-
-	std::unordered_map<char , int> freq_table;
-	std::ifstream file(filename+".hdr");
-	int swap = 0;
-	char character;
-	char keep;
-	int frequency;
-	while(!file.eof()){
-
-		file>>std::noskipws;
-		file>>character;
-		if(character != '&'){
-			keep = character;
-			swap = 0;
-		}
-		else if(character == '&' && swap == 0){
-			file>>frequency;
-			freq_table.insert(std::make_pair(keep,frequency));
-			swap = 1;
-		}
-	}
-	
-
-}
-
-
 //Huffman Tree implementation
 HuffmanTree::HuffmanTree(){};
 
