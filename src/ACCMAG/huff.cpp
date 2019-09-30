86a8ce3c6d03d4736d6280b64c18b816c3866cd8
Tapuwa-Dhliwayo <48251460+Tapuwa-Dhliwayo@users.noreply.github.com>
2019-09-30 12:33:14 +0200

    function implementation added
    Old huffman implementation removed

--- src/ACCMAG/huff.cpp
+++ src/ACCMAG/huff.cpp
@@ -1,73 +1,105 @@
 //Huffman Compression source file
 
 #include "huff.h"
-#include <iostream>
-using namespace std;
 
-void Node::fill(string* codebook, string &code){
-	if(!left && !right){
-		codebook[data] = code;
-		return;
+
+//Function Implementations
+std::vector<char> char_reader(std::string input){
+
+	input = input + ".txt";
+
+	std::ifstream file;
+	char hold;
+
+	file.open(input);
+
+	if(!file){
+		std::cout<<"File could not be opened."<<std::endl;
 	}
-	if(left){
-		code += '0';
-		left->fill(codebook, code);
-		code.erase(code.end()-1);
+	//Writes charatcers in the file into the char vector ignores all spaces tabs and newlines and generates a continuous stream of text as a vector of chars
+	std::vector<char> characters;
+	while(!file.eof()){
+
+		file>>hold>>std::ws;
+		characters.push_back(hold);
+
 	}
-	if(right){
-		code += '1';
-		right->fill(codebook, code);
-		code.erase(code.end()-1);
+
+	file.close();
+
+	return characters;
+}
+
+std::unordered_map<char, int> freq_generator(std::vector<char> characters){
+
+	std::unordered_map<char , int> freq_table;
+
+	auto ref = characters.begin();
+	int count = 0;
+	for(int i =0; i < (signed int)characters.size(); i++){
+
+		//Counts how many times the char appears in the the array
+		for(int j = 0; j < (signed int)characters.size(); j++){
+
+			if(characters.at(j) == *(ref+i)){
+				count++;
+			}
+		}
+		//Only added the char and its freq if there is no instance of the character present in the frequency table
+		if(freq_table.count(*(ref+i)) == 0){
+
+			//std::cout<<*(ref+i)<<" -> "<<count<<std::endl;
+			freq_table.insert(std::make_pair(*(ref+i),count));
+		}
+		count = 0;
 	}
+
+	return freq_table;
 }
 
-Node::Node(Node *r,Node *l){
-	freq = r->freq + l->freq;
-	right = r;
-	left = l;
-	min = (r->min < l->min) ? r->min : l->min;
+std::string encoder(std::vector<char> charater_order, std::unordered_map<char , std::string> code_table){
+
+	std::string encoded_data = "";
+	//Geenerates the encoded data by looping over the order of the char vector present 
+	//and writing the matching code to the present char in the loop if they match
+
+	for(auto& order: charater_order){
+		
+		for(auto& trace: code_table){
+
+			if(order == trace.first){
+
+				encoded_data = encoded_data + trace.second;
+			}
+		}
+	}
+
+	return encoded_data;
 
 }
 
-void Heap:: push(Node *newNode) {
-    int currentHeapNode = ++heapSize;
-    while (currentHeapNode != 1 && *minHeap[currentHeapNode / 2] > *newNode) {
-        minHeap[currentHeapNode] = minHeap[currentHeapNode / 2];
-        currentHeapNode = currentHeapNode / 2;
-    }
-    minHeap[currentHeapNode] = newNode;
-}
+void writer(std::string output, std::string encoded_data, std::unordered_map<char , std::string> code_table){
 
-void Heap:: pop(){
-    Node *lastNode = minHeap[heapSize];
-    minHeap [heapSize--] = minHeap[1];
-    int currentHeapNode = 1;
-    int child = 2;
-    
-    while (child <= heapSize) {
-        if (child < heapSize && *minHeap[child] > *minHeap[child + 1])
-            child++;
+	int len = encoded_data.length();
+	char buffer[len + 1];
+	//Byte array - buffer of the encoded data has been generated
+	strcpy(buffer, encoded_data.c_str());
 
-        if (*minHeap[child] > *lastNode)
-            break;
+	//Writing out the endcoded data array to an output file
+	std::ofstream out_file(output,std::ofstream::binary);
+	out_file<<buffer;
+	out_file.close();
 
-        minHeap[currentHeapNode] = minHeap[child];
-        currentHeapNode = child;
-        child *= 2;
-    } // while not at end of heap
-    
-    minHeap[currentHeapNode] = lastNode;
-}
+	//Creating a file output.hdr
+	std::string header_name = output + ".hdr";
 
-bool Node::operator> (const Node &rhs){
-	if(freq > rhs.freq)
-        	return true;
-    	if(freq < rhs.freq)
-        	return false;
-    	if(freq == rhs.freq)
-        	if(min > rhs.min)
-            		return true;
-    	return false;
+	//Writes the code table to the hdr file in the form 'char' -> 'code_symbol'
+	std::ofstream out_file2(header_name);
+	for(auto &code: code_table){
+		out_file2<<code.first<<" -> "<<code.second<<"\n";
+	}
+	out_file2.close();
+
 }
 
 
